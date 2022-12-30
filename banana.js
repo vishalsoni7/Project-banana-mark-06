6e74ff01efed7c8ac24c57aa1becde728de6e817
vishal soni <vishsoni043@gmail.com>
2022-12-30 23:44:25 +0530

    refactor
    correction in html code

--- banana.js
+++ banana.js
@@ -2,13 +2,9 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-// var serverURL = "https://api.funtranslations.com/translate/minion.json"
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-// var serverURL = "https://api.funtranslations.com/translate/"
-
-// var serverURL = "https://api.funtranslations.com/translate/minion.json"
-
-var serverURL = "https://api.funtranslations.com/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 
 function getTranslationURL(input) {
@@ -22,13 +18,14 @@ function errorHandler(error) {
 
 
 function clickHandler() {
-    var inputText = txtInput.value; 
+    var inputText = txtInput.value; // taking input
 
+    // calling server for processing
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
             var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText; 
+            outputDiv.innerText = translatedText; // output
            })
         .catch(errorHandler)
 };
